3905a5a3fe6d8f0785f0d9895a8ef52c7d282964
Kartheek Akella <sukruthkartheek@gmail.com>
2023-09-06 16:56:29 +0530

    console log

--- dist/swifto.js
+++ dist/swifto.js
@@ -59,4 +59,5 @@ async function fetchEngaged(swiftoId, textAndVariantIdList) {
 
 // Start the process when the page loads
 const swiftoId = window.SWIFTO_ID;
+console.log(swiftoId);
 fetchNewVisit();
